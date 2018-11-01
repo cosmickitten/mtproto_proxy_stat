cfbf5bf244e3634ac42463bcd139bd08463cf2ac
cosmickitten <44670418+cosmickitten@users.noreply.github.com>
2018-11-01 18:43:22 +0300

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build --rm -t $DOCKER_REPO  . -
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push $DOCKER_REPO
+docker push "$DOCKER_USERNAME"/"$DOCKER_REPO"
