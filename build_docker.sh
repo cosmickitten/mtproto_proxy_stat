fc867b0d540814422c375f75727eaa87b8baa844
cosmickitten <44670418+cosmickitten@users.noreply.github.com>
2018-11-01 17:16:11 +0300

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker build --rm .
+docker build --rm -t cosmickitten/tgproxyservice  . -
 echo "jkug9uh4y" | docker login -u "cosmickitten" --password-stdin
 docker push cosmickitten/tgproxyservice
